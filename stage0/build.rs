5669c3d1e895a704e5384adbab32b860d878b278
Putta Khunchalee <ultimaweapon@outlook.com>
2023-09-21 07:07:43 +0700

    Links to libstdc++ statically on Linux

--- stage0/build.rs
+++ stage0/build.rs
@@ -1,4 +1,14 @@
 fn main() {
+    // Static link to libstdc++ on Linux.
+    let profile = std::env::var("PROFILE").unwrap();
+    let os = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+
+    if profile == "release" && os == "linux" {
+        println!("cargo:rustc-link-arg-bins=-static-libgcc");
+        println!("cargo:rustc-link-arg-bins=-static-libstdc++");
+    }
+
+    // Link FFI library.
     let ffi = std::env::var("NITRO_FFI").unwrap();
 
     println!("cargo:rustc-link-search={}", ffi);
