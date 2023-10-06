c9ec5248810c89c7696fe9d7b1d922be5056e2c0
Putta Khunchalee <ultimaweapon@outlook.com>
2023-10-06 12:08:07 +0700

    Produces macOS universal binary on CI

--- build-cli.sh
+++ build-cli.sh
@@ -18,8 +18,30 @@ cmake --build "$root/ffi/build" --config "$type"
 cmake --install "$root/ffi/build" --config "$type"
 
 # cli
+mkdir -p "$root/dist/bin"
+
 if test x"$type" = x"Release"; then
-  cargo install --path stage0 --root dist
+  arg="--release"
+  dir="release"
 else
-  cargo install --path stage0 --root dist --debug
+  dir="debug"
+fi
+
+cargo build --manifest-path "$root/stage0/Cargo.toml" $arg
+
+if test x"$CMAKE_OSX_ARCHITECTURES" = x""; then
+  cp "$root/stage0/target/$dir/nitro" "$root/dist/bin"
+elif test x"$CMAKE_OSX_ARCHITECTURES" != x"x86_64;arm64"; then
+  echo "The value of CMAKE_OSX_ARCHITECTURES environment variable must be x86_64;arm64." 1>&2
+  exit 1
+elif test x"$(uname -m)" != x"x86_64"; then
+  echo "CMAKE_OSX_ARCHITECTURES environment variable is not supported on Apple Silicon." 1>&2
+  exit 1
+else
+  cargo build --manifest-path "$root/stage0/Cargo.toml" $arg --target aarch64-apple-darwin
+  lipo \
+    -create \
+    -output "$root/dist/bin/nitro" \
+    "$root/stage0/target/$dir/nitro" \
+    "$root/stage0/target/aarch64-apple-darwin/$dir/nitro"
 fi
